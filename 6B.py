28379fb870146fac8661217b3238d4b956953daa
varunesh.S <varuneshs20@gmail.com>
2026-08-18 15:55:06 +0530

    Update 6B.py

--- 6B.py
+++ 6B.py
@@ -1,23 +1,27 @@
 from collections import deque
 
-
-def is_palindrome(a):
-    dq = deque()
-
-    for ch in a:
-        if ch.isalnum():
-            dq.append(ch.lower())
-
-    while len(dq) > 1:
-        if dq.popleft() != dq.pop():
-            return False
-
-    return True
-
-
-a = input("Enter a string: ")
-
-if is_palindrome(a):
-    print("Palindrome")
-else:
-    print("Not a Palindrome")
+def palindrome(input_string):
+    cleaned_chars = [char.lower() for char in input_string if char.isalnum()]
+    cleaned_string = "".join(cleaned_chars)
+    print(f'output: "{cleaned_string}"')
+   
+    char_deque = deque(cleaned_chars)
+    is_palindrome = True
+   
+    while len(char_deque) > 1:
+        if char_deque.popleft() != char_deque.pop():
+            is_palindrome = False
+            break
+           
+    if is_palindrome:
+        print("Yes it's a palindrome")
+    else:
+        print("No it's not a palindrome")
+while True:
+    print("\n\nEnter break to Exit..")
+    ui = input("Enter a string: ")
+    if ui=="break":
+        print("Exiting....")
+        break
+    else:
+        palindrome(ui)
